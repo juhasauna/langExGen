3b329cf976a628b1129016ca4673d6dca5a5202e
juhasauna <juha.saunaaho@gmail.com>
2022-11-20 09:41:07 +0200

    added texts n shiit

--- sentences.go
+++ sentences.go
@@ -1,12 +1,18 @@
 package main
 
 type sentences struct {
-	test           string
-	fornam         string
-	musikPåDistans string
+	test                   string
+	fornam                 string
+	musikPåDistans         string
+	enDagIAmbulansen       string
+	undantagstillstånd     string
+	småferetagIEnLitenStad string
 }
 
 func getSentences() sentences {
+	// skämtat kring - joke about
+	// redskap - tools
+	// döpte - babtized
 	fornam := `_Berättelser om förnamn. __Enligt namnlagen ska alla ha ett förnamn
 	, och förnamnen får vara högst fyra till antalet. Många har traditionsenligt fått sitt namn, för att hedra minnet av släktingar som har gått bort. Det är också vanligt att namnen följer tidsandan. Ändå tror många föräldrar att de väljer unika namn för sina barn. Senast när barnet börjar på dagis märker de plötsligt att namnet är allmänt populärt, __och där finns fyra Emmor; tre Linneor; fem William, __och alla i personalen heter Monica.
 
@@ -25,29 +31,75 @@ func getSentences() sentences {
 	_Mamma till Milja:
 	Jag bestämde mig för länge sedan att om jag får ett barn, ska hen kunna säga namnet i telefon till någon utan att behöva göra en massa påpekanden. Jag måste till exempel alltid säga "Jag heter Erika med ett e och k", och sedan har jag ett efternamn som inte är det lättaste heller. Jag ville undvika att mitt barn ska råka ut för samma sak. Så när jag fick min dotter döpte vi henne till det finaste namnet jag vet. _Hon heter Milja.`
 
+	// bidrag - contribution
+	// Undantagstillstånd - state of emergency
+	// nätuppkopplingar - network connections
+	// stämmor - parts
+	// hade bett - had asked
+	// lades  posted
+	// telefonbandningar - phone tapes
+	// åstadkomma - achieve
 	musikPåDistans := `Vasa övningsskolas gymnasiekör har hoppat på trenden med körsång på distans nu i coronatider. Gymnasiekören har spelat in en egen version av Aftonens sång.
 
 	Medlemmarna har spelat in sina egna stämmor hemma och allt har mixats ihop till en musikvideo som nu finns på Youtube.
 	
-	Johan Sundqvist, timlärare i musik vid Vasa övningsskola, berättar att gymnasiets tidigare rektor Ulla Granfors tog kontakt och frågade om det fanns någon inspelning av Aftonens sång som kunde spelas på hennes pappa Thor Österholms begravning. Thor Österholm, som avled i mars, var tidigare rektor för Vasa övningsskola.
+	__Johan Sundqvist; timlärare i musik vid Vasa övningsskola, berättar att gymnasiets tidigare rektor Ulla Granfors tog kontakt, och frågade om det fanns någon inspelning av Aftonens sång, som kunde spelas på hennes pappa Thor Österholms begravning. __Thor Österholm; som avled i mars, __var tidigare rektor för Vasa övningsskola.
 	
-	Vi hade några inspelningar från skolfester men de hade dålig inspelningskvalitet, _säger Sundqvist.
+	Vi hade några inspelningar från skolfester men de hade dålig inspelningskvalitet; säger Sundqvist.
 	
-	Inspirerad av andra skolkörer som under undantagstillståndet har spelat in verk på distans kom Sundqvist fram till att man kunde pröva samma koncept med Vasa övningsskolas gymnasiekör.
+	Inspirerad av andra skolkörer som under undantagstillståndet har spelat in verk på distans, kom Sundqvist fram till att man kunde pröva samma koncept med Vasa övningsskolas gymnasiekör.
 
 	Aftonens sång är en lugn och stillsam sång som egentligen är idealisk som virtuellt projekt.
 	
 	_Spelades in med mobiltelefoner.
-	När en hel kör ska spela in en video på distans görs det inte i realtid eftersom det är svårt att tajma allas sång på grund av varierande nätuppkopplingar. Därför spelade de 36 medlemmarna i gymnasiekören in sina egna stämmor hemma med sina mobiltelefoner.
+	När en hel kör ska spela in en video på distans görs det inte i realtid, eftersom det är svårt att tajma allas sång på grund av varierande nätuppkopplingar. Därför spelade de 36 medlemmarna i gymnasiekören in sina egna stämmor hemma med sina mobiltelefoner.
 	
-	Till sin hjälp hemma hade studerandena dels en metronom som Sundqvist spelat in för att ange tempot, och dels de olika stämmorna som separata ljudfiler. Sedan mixades allas bidrag ihop till en helhet.
+	Till sin hjälp hemma hade studerande-na dels en metronom som Sundqvist spelat in för att ange tempot, och dels de olika stämmorna som separata ljudfiler. Sedan mixades allas bidrag ihop till en helhet.
 	
 	Sundqvist hade bett alla i kören klä sig i svart eftersom videon skulle spelas på Thor Österholms begravning. Efter begravningen lades videon också ut på Youtube där den fått positiv respons.
 	
 	Både Sundqvist och medlemmarna i gymnasiekören är mer än nöjda med slutresultatet.
 	
-	Jag trodde inte att man med telefonbandningar kunde åstadkomma något sådant här. Det var en intensiv process på ett par veckor där vi lärde oss mycket, både jag och studerandena.
+	Jag trodde inte att man med telefonbandningar kunde åstadkomma något sådant här. Det var en intensiv process på ett par veckor där vi lärde oss mycket, __både jag och studerande-na.
 	`
-	return sentences{test: "Geocaching är modern skattsökning som utövas internationellt. Med hjälp av gps-mottagare eller karta söker man efter skatter som geocachare har gömt i en ask på intressanta ställen.", fornam: fornam, musikPåDistans: musikPåDistans}
+
+	// vid något skede ändrade jag mig - at some stage changed my mind
+	// oroliga anhöriga - worried relatives
+	// hårfärgningen - hair dye
+	// blodförgiftning - blood poisoning
+	enDagIAmbulansen := `
+	_Erblind Mustafa är ambulanssköterska. Direkt efter examen som sjuksköterska fick han jobb på akuten i Helsingborg, men visste redan då att han ville läsa vidare till ambulanssköterska. 
+	
+	Ambulansjobbet med högt och hektiskt tempo har alltid lockat mig. Tidigare vill jag bli polis men vid något skede ändrade jag mig. Jag gillar att komma till jobbet och inte veta vilka typer av patienter jag kommer att möta, _säger Erblind.
+
+	Vi som jobbar i ambulansen har ofta mycket att göra hela arbetspasset, men vi har bara en patient åt gången och därför blir det automatisk lugnare än på akutmottagningen. Framkörningstiden till följande patient gör också att det finns lite tid för vila. Man behöver det lugnet för att kunna jobba när det sedan gäller, _berättar han.
+	
+	Inom ambulanssjukvården är det viktigt att kunna ställa om snabbt, __ena stunden kan det vara lugnt, men så händer plötsligt något och då måste man agera. Inte minst måste man kunna hantera oroliga anhöriga. __Personlighet spelar stor roll i yrket.
+	
+	Dagens första larm går och Erblind Mustafa hoppar in i ambulansen. En 85-årig kvinna; Dagmar; har värk i ryggen. Efter några undersökningar vill Erblind även testa hjärtats aktivitet. _Pulsen är normal. Ryggvärken kan ändå vara ett tecken på någon osynlig sjukdom, __och därför rekommenderar jag vidare undersökningar senare. Den här gången behöver du inte följa med in till sjukhuset, _konstaterar han. 
+	
+	Fint; Jag har så många hushållssysslor att göra. Och då kan jag också behålla min tid för hårfärgningen på fredag, _säger Dagmar lättat.
+	
+	På stationen blir det lite tid över mellan larmen. Kollegerna samlas i köket och har roligt tillsammans. Dagens sista larm gäller en man som har tagit för många tabletter. Medan mannen magpumpas på akutmottagningen i akutrum 1, kommer det in en äldre kvinna med blodförgiftning i akutrum 2. Med bara en skärm mellan sig kämpar båda för sina liv.
+	`
+
+	undantagstillstånd := `Undantagstillstånd är temporära rättsliga tillstånd när statsmakten tillfälligtvis ger sig själv utökade befogenheter för att ta kontroll över svåra eller farliga inhemska nödlägen, i synnerhet när dessa befogenheter inskränker människors friheter. Under ett undantagstillstånd sätts med andra ord de ordinära konstitutionella förfarandena ur spel, i syfte att återupprätta ordningen vid fara för rikets säkerhet, naturkatastrofer, eller andra nationella olyckstillbud. Om en stat formellt inför undantagstillstånd, vilket inte alla länder har konstitutionell möjlighet till, på nationell eller lokal nivå är det fråga om ett mycket akut nödläge.
+
+	Termen "undantagstillstånd" kan emellertid också avse mindre allvarliga nödlägen som föranleder statsmakter att tillfälligt införa restriktioner mot människors friheter och rättigheter. Undantagstillstånd är ett etablerat rättsbegrepp för särskilda förhållanden under nationella nödlägen. Olika länders lagstiftningar kan emellertid också tala om nödtillstånd, kris, extraordinär händelse, katastroftillstånd, larmberedskap, belägringstillstånd, krigsrätt och krigstillstånd för delvis samma lägen.
+	`
+	return sentences{test: "Geocaching är modern skattsökning som utövas internationellt. Med hjälp av gps-mottagare eller karta söker man efter skatter som geocachare har gömt i en ask på intressanta ställen.", fornam: fornam, musikPåDistans: musikPåDistans, enDagIAmbulansen: enDagIAmbulansen, undantagstillstånd: undantagstillstånd, småferetagIEnLitenStad: småferetagIEnLitenStad}
 
 }
+
+const småferetagIEnLitenStad = `
+__Läs reklamtexterna för fyra småföretag i en liten stad. __Välj det företag som bäst passar ihop med varje text. __Två alternativ är överflödiga.
+_Vilket företag beskriver texten bäst? _Klädbutik; Inredningsbutik; Idrottscentrum; Skönhetssalong; Fastighetsmäklare; Campingplats.
+
+Vi ligger nära havet; naturen och den populära Skärgårdens ringväg. Hos oss kan du övernatta i tält eller husvagn eller hyra en stuga. Stadens allmänna badstrand finns i närheten. Också stadscentrum med aktiviteter såsom äventyrsgolf, tennis; frisbeegolf och ridning finns på promenad och cykelavstånd. _Välkommen!
+
+Som företagare vill jag hålla mig uppdaterad om de nyaste behandlingsformerna, _apparaterna och produkterna. Det är viktigt att kunderna betjänas med behandlingar och vård som är anpassade för deras individuella behov. På det sättet kan jag erbjuda mina kunder en avkopplande stund i vardagen. Vi säljer också hudvårdsprodukter av hög kvalitet. Boka tid och njut av lite lyx!
+
+Vårt mål är att erbjuda produkter där kvalitet; design och komfort går hand i hand. Vi väljer bara kvalitetsmärken till vårt sortiment, och hos oss hittar du alternativ för både vardag och fest. _Urvalet är stort, också då det gäller storlekar och modeller som passar olika kroppsformer och kunder i olika åldrar. __Vill du ha personlig och vänlig service, _kom till oss!
+
+_Är du trött på ditt hem? __Saknar du något nytt runt dig? Vi kan hjälpa dig både när du planerar en större renovering, och när du söker något mindre som kan pigga upp dig i vardagen. Vi tycker att föremål ska vara vackra men gärna ha en funktion. __Vi tycker också om att blanda olika stilar. Kom och bolla dina idéer med oss så hittar vi en bra lösning för dig!
+`
--- textToSpeech.go
+++ textToSpeech.go
@@ -22,12 +22,6 @@ type inputItem struct {
 	audioContent []byte
 }
 
-type audioAndPause struct {
-	audio   inputItem
-	pause   inputItem
-	repeats int
-}
-
 func generateMp3() error {
 	ctx := context.Background()
 	opt := option.WithCredentialsFile("C:/Users/FIJUSAU/go/src/firebase/serviceAccount.json")
@@ -37,41 +31,40 @@ func generateMp3() error {
 		return err
 	}
 
-	sentence := getSentences().test
+	sentence := getSentences().småferetagIEnLitenStad
 	sentence = strings.ReplaceAll(sentence, "\t", "")
 	sentence = strings.ReplaceAll(sentence, "\n", "")
 	seps := regexp.MustCompile(`[,|.|:|?|!]`)
 	splitIt := seps.Split(sentence, -1)
-	data := []audioAndPause{}
+	data := []inputItem{}
 	for i, v := range splitIt {
 		if v == "" {
 			continue
 		}
 		audio := inputItem{sentence: v, id: fmt.Sprintf("%d_1", i), voice: "sv-SE-Wavenet-A"}
-		data = append(data, audioAndPause{audio: audio})
+		data = append(data, audio)
 	}
 	var audioContent []byte
 	for _, v := range data {
 		count := 3
-		rmWhiteSpace := strings.TrimSpace(v.audio.sentence)
+		rmWhiteSpace := strings.TrimSpace(v.sentence)
 		if strings.Index(rmWhiteSpace, "_") == 0 {
-			count = strings.Count(v.audio.sentence, "_")
-			v.audio.sentence = strings.ReplaceAll(v.audio.sentence, "_", "")
+			count = strings.Count(v.sentence, "_")
+			v.sentence = strings.ReplaceAll(v.sentence, "_", "")
 		}
 
-		reqAudio := audioRequest(v.audio)
+		reqAudio := audioRequest(v)
 
 		respAudio, err := client.SynthesizeSpeech(ctx, &reqAudio)
 		if err != nil {
 			return err
 		}
-		fmt.Println(len(respAudio.AudioContent))
-		quietAudio := makeQuietAudio(len(respAudio.AudioContent) * 3 / 2)
+		quietAudio := makeQuietAudio(len(respAudio.AudioContent))
 		for i := 0; i < count; i++ {
 			audioContent = append(audioContent, respAudio.AudioContent...)
 			audioContent = append(audioContent, quietAudio...)
 		}
-		fmt.Printf("Wrote audio to []byte: %v\n", v.audio.id)
+		fmt.Printf("Wrote audio to []byte: %v\n", v.id)
 	}
 	path := "C:/Users/FIJUSAU/go/src/langExGen/audio/"
 	filePath := path + "test.mp3"
@@ -84,13 +77,13 @@ func generateMp3() error {
 }
 
 func makeQuietAudio(length int) []byte {
+	length = int(float64(length) * 1.6)
 	bytes, err := os.ReadFile("C:/Users/FIJUSAU/go/src/langExGen/audio/space.mp3")
 	if err != nil {
 		log.Fatal(err)
 	}
 	spaceLen := len(bytes)
 	quietLen := length/spaceLen + 1
-	fmt.Println(spaceLen, "\t", quietLen)
 	var quietBytes []byte
 	for i := 0; i < quietLen; i++ {
 		quietBytes = append(quietBytes, bytes...)
@@ -115,115 +108,4 @@ func audioRequest(v inputItem) texttospeechpb.SynthesizeSpeechRequest {
 		},
 	}
 	return req
-}
-
-func generateQuiet() error {
-	ctx := context.Background()
-	opt := option.WithCredentialsFile("C:/Users/FIJUSAU/go/src/firebase/serviceAccount.json")
-
-	client, err := texttospeech.NewClient(ctx, opt)
-	if err != nil {
-		return err
-	}
-
-	type volRate struct {
-		volume       float64
-		speakingRate float64
-	}
-
-	data := []audioAndPause{}
-
-	audio := inputItem{sentence: " ", voice: "sv-SE-Wavenet-A"}
-	data = append(data, audioAndPause{audio: audio})
-
-	var audioContent []byte
-	for _, v := range data {
-
-		reqAudio := audioRequest(v.audio)
-
-		respAudio, err := client.SynthesizeSpeech(ctx, &reqAudio)
-		if err != nil {
-			return err
-		}
-
-		audioContent = append(audioContent, respAudio.AudioContent...)
-		fmt.Printf("Wrote audio to []byte: %v\n", v.audio.id)
-	}
-	path := "C:/Users/FIJUSAU/go/src/langExGen/audio/"
-	filePath := path + "test.mp3"
-	err = os.WriteFile(filePath, audioContent, 0644)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func GenerateCustomMp3_old() error {
-	ctx := context.Background()
-	opt := option.WithCredentialsFile("C:/Users/FIJUSAU/go/src/firebase/serviceAccount.json")
-
-	client, err := texttospeech.NewClient(ctx, opt)
-	if err != nil {
-		return err
-	}
-
-	type volRate struct {
-		volume       float64
-		speakingRate float64
-	}
-	// normal := volRate{0, 1}
-	pause := volRate{-96, .6}
-
-	sentence := getSentences().test
-	sentence = strings.ReplaceAll(sentence, "\t", "")
-	sentence = strings.ReplaceAll(sentence, "\n", "")
-	seps := regexp.MustCompile(`[,|.|:|?|!]`)
-	splitIt := seps.Split(sentence, -1)
-	data := []audioAndPause{}
-	for i, v := range splitIt {
-		if v == "" {
-			continue
-		}
-		audio := inputItem{sentence: v, id: fmt.Sprintf("%d_1", i), voice: "sv-SE-Wavenet-A"}
-		pause := inputItem{sentence: v, id: fmt.Sprintf("%d_1pause", i), voice: "sv-SE-Wavenet-A", volume: pause.volume, speakingRate: pause.speakingRate}
-		data = append(data, audioAndPause{audio: audio, pause: pause})
-	}
-	var audioContent []byte
-	for _, v := range data {
-		count := 3
-		rmWhiteSpace := strings.TrimSpace(v.audio.sentence)
-		if strings.Index(rmWhiteSpace, "_") == 0 {
-			count = strings.Count(v.audio.sentence, "_")
-			v.audio.sentence = strings.ReplaceAll(v.audio.sentence, "_", "")
-			v.pause.sentence = strings.ReplaceAll(v.pause.sentence, "_", "")
-		}
-
-		reqAudio := audioRequest(v.audio)
-		reqPause := audioRequest(v.pause)
-
-		respAudio, err := client.SynthesizeSpeech(ctx, &reqAudio)
-		if err != nil {
-			return err
-		}
-		respPause, err := client.SynthesizeSpeech(ctx, &reqPause)
-		if err != nil {
-			return err
-		}
-		fmt.Println(len(respAudio.AudioContent))
-		fmt.Println(len(respPause.AudioContent))
-		for i := 0; i < count; i++ {
-			audioContent = append(audioContent, respAudio.AudioContent...)
-			audioContent = append(audioContent, respPause.AudioContent...)
-		}
-		fmt.Printf("Wrote audio to []byte: %v\n", v.audio.id)
-	}
-	path := "C:/Users/FIJUSAU/go/src/langExGen/audio/"
-	filePath := path + "test.mp3"
-	err = os.WriteFile(filePath, audioContent, 0644)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
